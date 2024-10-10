ea13009a0be92bd1c3962ea68dad38e22e919375
SIST-101 <SIST-101@SIST>
2024-10-10 16:12:45 +0900

    1111

---
+++ SpringLastProject/src/main/java/com/sist/service/FreeBoardServiceImpl.java
@@ -0,0 +1,56 @@
+package com.sist.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.*;
+import com.sist.vo.*;
+import com.sist.dao.*;
+@Service
+public class FreeBoardServiceImpl implements FreeBoardService{
+    @Autowired
+    private FreeBoardDAO fDao;
+
+	@Override
+	public List<FreeBoardVO> freeboardListData(int start, int end) {
+		// TODO Auto-generated method stub
+		return fDao.freeboardListData(start, end);
+	}
+	
+	@Override
+	public int freeboardRowCount() {
+		// TODO Auto-generated method stub
+		return fDao.freeboardRowCount();
+	}
+	
+	@Override
+	public void freeboardInsert(FreeBoardVO vo) {
+		// TODO Auto-generated method stub
+		fDao.freeboardInsert(vo);
+	}
+
+	@Override
+	public FreeBoardVO freeboardDetailData(int no) {
+		// TODO Auto-generated method stub
+		return fDao.freeboardDetailData(no);
+	}
+
+	@Override
+	public void freeboardDelete(int no) {
+		// TODO Auto-generated method stub
+		fDao.freeboardDelete(no);
+	}
+   
+    /*
+     *   JSP => .do
+     *    |
+     *   Controller 
+     *    |
+     *   Mapper
+     *    | 
+     *   DAO
+     *    |
+     *   Service 
+     *    |
+     *   ServiceImpl
+     */
+}
